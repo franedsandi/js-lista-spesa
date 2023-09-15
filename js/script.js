13c66f8f73928c6b7a25b61f20aba768d26cb06a
franedsandi <franedsandi@gmail.com>
2023-09-15 09:45:19 +0200

    list mode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* Step by step
 1. make an array with the grosseries list
-2. use a cicle while to print each element of the groseries list */
+2. use a cicle while to print each element of the groseries list in output until the list end*/
 const spesa = [
   'pane', 'latte', 'zucchero', 'biscotti', 'prosciutto', 'formaggio', 'pasta', 'sugo', 'aqcua'
 ];
@@ -8,7 +8,7 @@ let i = 0;
 
 while (i < spesa.length){
   prodotti = spesa[i];
-  document.getElementById('output').innerHTML += prodotti +'<br>';
+  document.getElementById('output').innerHTML +=`<li> ${prodotti} </li>`;
   console.log(spesa[i]);
   i++;
 }
